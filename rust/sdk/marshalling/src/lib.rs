55ed023b6c3026a347c77aca012345039982d206
Diego Ongaro <diego@loam.me>
2023-06-06 23:43:31 -0700

    marshalling: Fix doc test

--- rust/sdk/marshalling/src/lib.rs
+++ rust/sdk/marshalling/src/lib.rs
@@ -31,7 +31,7 @@ pub fn from_slice<T: DeserializeOwned>(bytes: &[u8]) -> Result<T, Deserializatio
 /// # Example
 ///
 /// ```
-/// use juicebox_sdk_core::marshalling::{from_slice, serialize_secret, to_vec};
+/// use juicebox_sdk_marshalling::{from_slice, serialize_secret, to_vec};
 /// use secrecy::{ExposeSecret, SecretString};
 /// use serde::{Deserialize, Serialize};
 ///
