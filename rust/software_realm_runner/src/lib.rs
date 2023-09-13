f2e2594cca2aa3b2547b13113e0d9f18c6ffa9fb
simon <simon@juicebox.me>
2023-09-13 15:21:00 -0700

    fix potential port conflicts in software_realm tests

--- rust/software_realm_runner/src/lib.rs
+++ rust/software_realm_runner/src/lib.rs
@@ -14,13 +14,14 @@ pub struct RunnerArgs {
 pub struct Runner;
 
 impl Runner {
-    pub async fn run(pg: &mut ProcessGroup, args: &RunnerArgs) {
+
+    // Returns true if the software realm was successfully started.
+    pub async fn run(pg: &mut ProcessGroup, args: &RunnerArgs) -> bool {
         println!(
             "Starting software realm with id: {:?}, port: {}",
             args.id, args.port
         );
-        pg.spawn(
-            Command::new("jb-sw-realm")
+        let mut child = Command::new("jb-sw-realm")
                 .arg("-id")
                 .arg(hex::encode(args.id.0))
                 .arg("-port")
@@ -29,16 +30,24 @@ impl Runner {
                 .env(
                     "TENANT_SECRETS",
                     serde_json::to_string(&args.secrets).unwrap(),
-                ),
-        );
+                ).spawn().expect("failed to launch jb-sw-realm");
+
         for _ in 0..100 {
             match reqwest::get(format!("http://0.0.0.0:{}", args.port)).await {
-                Ok(response) if response.status().is_success() => return,
+                Ok(response) if response.status().is_success() => {
+                    pg.add(child);
+                    return true;
+                }
                 _ => {
+                    if let Ok(Some(_exit_code)) = child.try_wait() {
+                        // The child process has already exited. This likely due
+                        // to a conflict on the port its trying to use
+                        return false;
+                    }
                     sleep(Duration::from_millis(10)).await;
                 }
             }
         }
-        panic!("repeatedly failed to connect to realm");
+        false
     }
 }
