80e8b9e62ded9a817f6a0e79cce2c977aa0efaf1
Nora Trapp <nora@juicebox.me>
2024-04-04 16:36:17 -0700

    Add support for AuthTokenGenerator to web-demo

--- javascript/web-demo/src/demo.js
+++ javascript/web-demo/src/demo.js
@@ -1,14 +1,43 @@
 import React, { useState } from 'react'
 import { Buffer } from 'buffer'
 
-const { Client, Configuration, RegisterError, DeleteError, RecoverErrorReason } = await import('juicebox-sdk');
+const { Client, Configuration, RegisterError, DeleteError, RecoverErrorReason, AuthTokenGenerator } = await import('juicebox-sdk');
 
 export const Demo = () => {
-  const [configJSON, setConfigJSON] = useState('{"realms":[], "register_threshold": 1, "recover_threshold": 1, "pin_hashing_mode": "Standard2019"}');
-  const [authTokensJSON, setAuthTokensJSON] = useState('{"realmId": "token"}');
+  const [configJSON, setConfigJSON] = useState(`{
+  "realms": [
+    {
+      "id": "9f105f0bf34461034df2ba67b17e5f43",
+      "address": "https://gcp.realms.juicebox.xyz/"
+    },
+    {
+      "id": "7546bca7074dd6af64a3c230f04ef803",
+      "address": "https://aws.realms.juicebox.xyz/"
+    },
+    {
+      "id": "44e18495c18a3c459954d73d2689e839",
+      "public_key": "f6ce077e253010a45101f299a22748cb613a83bd69458e4c3fd36bffdc3c066a",
+      "address": "https://lb.juicebox.xyz/"
+    }
+  ],
+  "register_threshold": 3,
+  "recover_threshold": 3,
+  "pin_hashing_mode": "Standard2019"
+}`);
+
+  const [authTokensGeneratorJSON, setAuthTokensGeneratorJSON] = useState(`{
+  "key": "302e020100300506032b65700422042070f4086a565233bd57bb577ddf7966d9d506e98e459eba6b4c521f04dd0f9d9c",
+  "tenant": "juiceboxdemo",
+  "version": 2
+}`);
+  const [authTokensMapJSON, setAuthTokensMapJSON] = useState(`{"realmId": "token"}`);
+  const authTokensJSON = () => authMode === 'generator' ? authTokensGeneratorJSON : authTokensMapJSON;
+  const setAuthTokensJSON = json => authMode === 'generator' ? setAuthTokensGeneratorJSON(json) : setAuthTokensMapJSON(json);
+  const [authMode, setAuthMode] = useState('generator'); // 'generator' or 'map'
+
   const [pin, setPin] = useState('1234');
-  const [info, setInfo] = useState('info');
-  const [secret, setSecret] = useState('secret');
+  const [info, setInfo] = useState('apollo');
+  const [secret, setSecret] = useState('artemis');
   const [allowedGuesses, setAllowedGuesses] = useState(2);
   const [output, setOutput] = useState('');
   const [isOperationInProgress, setOperationInProgress] = useState(false);
@@ -16,9 +45,16 @@ export const Demo = () => {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
+
   window.JuiceboxGetAuthToken = async (realmId) => {
-    const authTokens = JSON.parse(authTokensJSON);
-    return authTokens[Buffer.from(realmId).toString('hex')];
+    const realmIdString = Buffer.from(realmId).toString('hex');
+    if (authMode === 'generator') {
+      const generator = new AuthTokenGenerator(authTokensGeneratorJSON);
+      return generator.vend(realmIdString, realmIdString);
+    } else {
+      const authTokens = JSON.parse(authTokensMapJSON);
+      return authTokens[realmIdString];
+    }
   }
 
   const createClient = () => {
@@ -82,13 +118,21 @@ export const Demo = () => {
         <label>Config (JSON):</label>
       </div>
       <div>
-        <textarea rows={8} value={configJSON} onChange={(e) => setConfigJSON(e.target.value)} />
+        <textarea rows={20} cols={90} value={configJSON} onChange={(e) => setConfigJSON(e.target.value)} />
       </div>
       <div>
-        <label>API Tokens (JSON):</label>
+        <label>Auth Tokens (JSON):</label>
+        <span>
+          <input type="radio" id="generator" name="authMode" value="generator" checked={authMode === 'generator'} onChange={() => setAuthMode('generator')} />
+          <label>Token Generator</label>
+        </span>
+        <span>
+          <input type="radio" id="map" name="authMode" value="map" checked={authMode === 'map'} onChange={() => setAuthMode('map')} />
+          <label>Token Map</label>
+        </span>
       </div>
       <div>
-        <textarea rows={8} value={authTokensJSON} onChange={(e) => setAuthTokensJSON(e.target.value)} />
+        <textarea rows={5} cols={110} value={authTokensJSON()} onChange={(e) => setAuthTokensJSON(e.target.value)} />
       </div>
       <div>
         <label>PIN:</label>
